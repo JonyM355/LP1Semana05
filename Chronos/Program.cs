b206d2634b0163a74bbc656c40c61e89237255f3
JonyM355 <joaoricardoam05@gmail.com>
2024-03-23 17:54:56 +0000

    Show the elapsed time in the terminal

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Chronos
 {
@@ -6,7 +8,33 @@ namespace Chronos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stopwatch crono1;
+            Stopwatch crono2;
+            crono1 = new Stopwatch();
+            crono2 = new Stopwatch();
+            crono1.Start();
+            Thread.Sleep(600);
+            crono2.Start();
+            Thread.Sleep(200);
+            crono1.Stop();
+            crono2.Stop();
+            TimeSpan crono1_TimeSpan = crono1.Elapsed;
+            TimeSpan crono2_TimeSpan = crono2.Elapsed;
+
+            string crn1_ElapsedTime =
+            String.Format("{0:00}:{1:00}.{2:00}", 
+            crono1_TimeSpan.Minutes, 
+            crono1_TimeSpan.Seconds,
+            crono1_TimeSpan.Milliseconds / 10);
+
+            string crn2_ElapsedTime = 
+            String.Format("{0:00}:{1:00}.{2:00}",
+            crono2_TimeSpan.Minutes, 
+            crono2_TimeSpan.Seconds,
+            crono2_TimeSpan.Milliseconds / 10);
+
+            Console.WriteLine("RunTime crono1: " + crn1_ElapsedTime);
+            Console.WriteLine("RunTime crono2: " + crn2_ElapsedTime);
         }
     }
 }
