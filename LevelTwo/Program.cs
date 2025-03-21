7e10cff3bddcf8a08507c933240151d7863fd547
JonyM355 <joaoricardoam05@gmail.com>
2025-03-21 12:36:26 +0000

    Add method SoGoodMethod and use on project LevelTwo

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using SixLabors.ImageSharp;
 using Spectre.Console;
+using SoGoodLib;
 
 namespace LevelTwo
 {
@@ -8,6 +9,7 @@ namespace LevelTwo
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine(SoGoodClass.SoGoodMethod());
             if (args.Length == 2)
             {
                 CanvasImage image = new CanvasImage($"{args[0]}");
--- SoGoodLib/SoGoodClass.cs
+++ SoGoodLib/SoGoodClass.cs
@@ -4,6 +4,10 @@ namespace SoGoodLib
 {
     public class SoGoodClass
     {
-
+        public static string SoGoodMethod()
+        {
+            string sentence = "I came from another project!";
+            return sentence;
+        }
     }
 }
