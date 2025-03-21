bec187bfe6b08b19027f98003ff49514353be30c
JonyM355 <joaoricardoam05@gmail.com>
2025-03-21 12:22:37 +0000

    Project LevelTwo

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using Spectre.Console;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                CanvasImage image = new CanvasImage($"{args[0]}");
+                int size = int.Parse(args[1]);
+
+                image.MaxWidth(size);
+                AnsiConsole.Write(image);
+            }
+
+            else
+            {
+                CanvasImage image = new CanvasImage("tux.jpg");
+                image.MaxWidth(24);
+                AnsiConsole.Write(image); 
+            }
+        }
+    }
+}
