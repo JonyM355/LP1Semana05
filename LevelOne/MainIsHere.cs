936605e9d3813427bbefa0fa5ea8dbab87f679de
JonyM355 <joaoricardoam05@gmail.com>
2025-03-21 10:49:43 +0000

    New method

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -12,5 +12,10 @@ namespace LevelOne
         {
             Console.WriteLine("Hello LP1!");
         }
+
+        public static void NewMethod(string[] stri)
+        {
+            Console.WriteLine("Bye!");
+        }
     }
 }
